053d81f527652aaa150b6fa10c4c9fe44d545350
m-shrief <m.shrief.dev@proton.me>
2024-08-17 16:07:50 +0300

    Extracted and Encapsulated greeting apiinto its component, and its registeration.

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"go_huma_backend/router"
 	"net/http"
 
@@ -10,57 +8,12 @@ import (
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
 )
 
-// GreetingOutput represents the greeting operation response.
-type GreetingOutput struct {
-	Body struct {
-		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
-	}
-}
-
 func main() {
+	// Router & API
 	r := router.NewRouter()
 	router.UseMiddlewares()
-	api := router.InitAPI(r, huma.DefaultConfig("My API", "1.0.0"))
-
-	// // Register GET /greeting/{name} handler.
-	// huma.Get(
-	// 	api,
-	// 	"/greeting/{name}",
-	// 	func(
-	// 		ctx context.Context,
-	// 		input *struct {
-	// 			Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
-	// 		},
-	// 	) (*GreetingOutput, error) {
-	// 		resp := &GreetingOutput{}
-	// 		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-	// 		return resp, nil
-	// 	},
-	// )
-
-	// Register GET /greeting/{name} handler with more information for the docs.
-	huma.Register(
-		api,
-		// Use huma.Operation to add more information for the docs.
-		huma.Operation{
-			OperationID: "get-greeting",
-			Method:      http.MethodGet,
-			Path:        "/greeting/{name}",
-			Summary:     "Get a greeting",
-			Description: "Get a greeting for a person by name.",
-			Tags:        []string{"Greetings"},
-		},
-		func(
-			ctx context.Context,
-			input *struct {
-				Name string `path:"name" maxLength:"10" example:"world" doc:"Name to greet"`
-			},
-		) (*GreetingOutput, error) {
-			resp := &GreetingOutput{}
-			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-			return resp, nil
-		},
-	)
+	router.InitAPI(r, huma.DefaultConfig("My API", "1.0.0"))
+	router.RegisterAPIs()
 
 	// Start the server!
 	http.ListenAndServe("127.0.0.1:3000", r)
--- router/api.go
+++ router/api.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"go_huma_backend/internal/components/greeting"
+
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	"github.com/go-chi/chi/v5"
@@ -11,4 +13,8 @@ var API huma.API
 func InitAPI(router *chi.Mux, config huma.Config) huma.API {
 	API = humachi.New(router, config)
 	return API
+}
+
+func RegisterAPIs() {
+	greeting.RegisterAPI(API)
 }
