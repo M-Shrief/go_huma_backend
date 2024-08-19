872aa8e37ff884eccba164e6c82e289cbb808515
m-shrief <m.shrief.dev@proton.me>
2024-08-19 12:53:06 +0300

    Added Database ENVs.

--- internal/config/default.go
+++ internal/config/default.go
@@ -24,8 +24,24 @@ var (
 	PORT string
 )
 
+// Database
+var (
+	DB_HOST     string
+	DB_PORT     string
+	DB_NAME     string
+	DB_USER     string
+	DB_PASSWORD string
+)
+
 func assignValues() {
 	// General
 	HOST = envs["HOST"]
 	PORT = envs["PORT"]
+
+	// Database
+	DB_HOST = envs["DB_HOST"]
+	DB_PORT = envs["DB_PORT"]
+	DB_USER = envs["DB_USER"]
+	DB_NAME = envs["DB_NAME"]
+	DB_PASSWORD = envs["DB_PASSWORD"]
 }
