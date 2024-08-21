9258046d5ad8c3f4b4ff772cd1d53e20c53b2764
m-shrief <m.shrief.dev@proton.me>
2024-08-21 15:07:34 +0300

    Added logger module to use Zerolog around the project.

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/zerolog v1.33.0
 	golang.org/x/crypto v0.21.0
 	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
 )
@@ -16,6 +17,10 @@ require (
 	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 )
---
+++ logger/logger.go
@@ -0,0 +1,86 @@
+// Zerolog Config
+//
+// Logging levels guide:
+// TRACE (-1): for tracing the code execution path.
+// DEBUG (0): messages useful for troubleshooting the program.
+// INFO (1): messages describing the normal operation of an application.
+// WARNING (2): for logging events that need may need to be checked later.
+// ERROR (3): error messages for a specific operation.
+// FATAL (4): severe errors where the application cannot recover. os.Exit(1) is called after the message is logged.
+// PANIC (5): similar to FATAL, but panic() is called instead.
+package logger
+
+import (
+	"go_huma_backend/internal/config"
+	"io"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+var log zerolog.Logger
+
+func Init() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	var output io.Writer
+	var lvl zerolog.Level
+	if config.APP_ENV == "development" {
+		output = zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: time.RFC3339,
+		}
+		lvl = zerolog.DebugLevel
+	} else {
+		output = zerolog.MultiLevelWriter(os.Stderr)
+		lvl = zerolog.TraceLevel
+	}
+
+	log = zerolog.New(output).
+		Level(lvl).
+		With().
+		Timestamp().
+		Logger()
+	// Sample(zerolog.LevelSampler{
+	// TraceSampler: &zerolog.BasicSampler{N: 10},  // Log 1 out of every 10 TRACE messages
+	// DebugSampler: &zerolog.BasicSampler{N: 5},   // Log 1 out of every 5 DEBUG messages
+	// InfoSampler:  &zerolog.BasicSampler{N: 2},   // Log 1 out of every 2 INFO messages
+	// })
+
+}
+
+func Get() zerolog.Logger {
+	return log
+}
+
+// functions to shorten basic usage of zerolog,
+// Instead of using l := logger.Get()
+// then using it like: l.Info().Msg("...")
+// You can just call it directly: logger.Info().Msg("...")
+
+func Trace() *zerolog.Event {
+	return log.Trace()
+}
+
+func Debug() *zerolog.Event {
+	return log.Debug()
+}
+
+func Info() *zerolog.Event {
+	return log.Info()
+}
+
+func Warn() *zerolog.Event {
+	return log.Warn()
+}
+
+func Error() *zerolog.Event {
+	return log.Error()
+}
+
+func Panic() *zerolog.Event {
+	return log.Panic()
+}
